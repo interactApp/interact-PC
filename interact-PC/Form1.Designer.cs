3d9139af0caf8a406ea49542c1dfb45abb401496
ethan-bit <ethan-bit@protonmail.com>
2020-06-03 23:32:49 +0800

    ADD qrcoder

--- interact-PC/Form1.Designer.cs
+++ interact-PC/Form1.Designer.cs
@@ -28,20 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("宋体", 13F);
+            this.textBox1.Location = new System.Drawing.Point(205, 45);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(268, 27);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(497, 45);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 27);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "连接";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(753, 454);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- interact-PC/Form1.cs
+++ interact-PC/Form1.cs
@@ -21,5 +21,23 @@ namespace interact_PC
         {
 
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int version = Convert.ToInt16("SSSS");
+
+            int pixel = Convert.ToInt16("12");
+
+            string str_msg = "SSSS";
+
+            int int_icon_size = Convert.ToInt16("12");
+
+            int int_icon_border = Convert.ToInt16("1");
+
+            Bitmap bmp = chestnut_qrcode.Encoder.code(str_msg, version, pixel, "E:/seaconch/git/1.jpg", int_icon_size, int_icon_border);
+
+            pb_qrcode.Image = bmp;
+
+        }
     }
 }
